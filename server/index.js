0d937056082fb0fb77662998a2ec681a3f5a7e66
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-11-24 10:18:28 +0100

    User Routes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import authRoutes from "./routes/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from './controllers/auth.js'
 import { verifyToken } from './middleware/auth.js';
 
@@ -41,7 +42,9 @@ const upload = multer({ storage });
 
 app.post("auth/register" , upload.single("picture"), register);
 
-app.use("/auth" , authRoutes)
+
+app.use("/auth" , authRoutes);
+app.use("/users", userRoutes)
 
 const PORT = process.env.PORT || 6001;
 
